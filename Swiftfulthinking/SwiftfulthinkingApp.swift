24c14e967d124feca2d150bc200b7515ed1b2611
raden-dimas012 <dimascode012@gmail.com>
2021-10-22 09:27:40 +0700

    Initial Commit

---
+++ Swiftfulthinking/SwiftfulthinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulthinkingApp.swift
+//  Swiftfulthinking
+//
+//  Created by Raden Dimas on 22/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulthinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
